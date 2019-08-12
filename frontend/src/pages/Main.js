dcd37a3d7c34ef2c901ef58237f77a95c0b69377
Gabriela Carvalho Camilo <gabriela.camilo@pca.com.br>
2019-08-12 18:02:42 -0300

    Estili e estruturação da pág. Main

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,7 +1,79 @@
 import React from 'react'
+import './Main.css'
+
+import logo from '../assets/logo.svg';
+import dislike from '../assets/dislike.svg';
+import like from '../assets/like.svg';
 
 export default function Main({match}){// o react router dom inclui uma propriedade chamada match, que possui todos os parâmetros passados para esta rota
   return (
-    <h1>{match.params.id}</h1>
+    <div className="main-container">
+      <img src={logo} alt="Tindev"/>
+      <ul>
+        <li>
+          <img src="https://avatars0.githubusercontent.com/u/3604053?v=4" alt="Tindev"/>
+          <footer>
+            <strong>John Doe</strong>
+            <p>lalsjaisja lajska ska sjajskajskaska ksak</p>
+          </footer>
+          <div className="buttons">
+            <button type="button">
+              <img src={dislike} alt="Dislike"/>
+            </button>
+            <button type="button">
+              <img src={like} alt="Like"/>
+            </button>
+          </div>
+        </li>
+
+        <li>
+          <img src="https://avatars0.githubusercontent.com/u/3604053?v=4" alt="Tindev"/>
+          <footer>
+            <strong>John Doe</strong>
+            <p>lalsjaisja lajska ska sjajskajskaska ksak</p>
+          </footer>
+          <div className="buttons">
+            <button type="button">
+              <img src={dislike} alt="Dislike"/>
+            </button>
+            <button type="button">
+              <img src={like} alt="Like"/>
+            </button>
+          </div>
+        </li>
+
+        <li>
+          <img src="https://avatars0.githubusercontent.com/u/3604053?v=4" alt="Tindev"/>
+          <footer>
+            <strong>John Doe</strong>
+            <p>lalsjaisja lajska ska sjajskajskaska ksak</p>
+          </footer>
+          <div className="buttons">
+            <button type="button">
+              <img src={dislike} alt="Dislike"/>
+            </button>
+            <button type="button">
+              <img src={like} alt="Like"/>
+            </button>
+          </div>
+        </li>
+
+        <li>
+          <img src="https://avatars0.githubusercontent.com/u/3604053?v=4" alt="Tindev"/>
+          <footer>
+            <strong>John Doe</strong>
+            <p>lalsjaisja lajska ska sjajskajskaska ksak</p>
+          </footer>
+          <div className="buttons">
+            <button type="button">
+              <img src={dislike} alt="Dislike"/>
+            </button>
+            <button type="button">
+              <img src={like} alt="Like"/>
+            </button>
+          </div>
+        </li>
+      </ul>
+    </div>
   )
 }
